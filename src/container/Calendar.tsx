c6d960da72e534b0ae72db656cd7dac0eaa954b0
kay0829 <eluka97@daum.net>
2023-06-22 10:37:32 +0900

    🔧 0) tailwindcss 기본 설정

--- src/container/Calendar.tsx
+++ src/container/Calendar.tsx
@@ -1,7 +1,7 @@
 function Calendar () {
     return (
         <>
-            <p className="text-indigo-600">hello</p>
+            <h1 className="text-3xl font-bold underline">Hello world!</h1>
         </>
     )
 }
