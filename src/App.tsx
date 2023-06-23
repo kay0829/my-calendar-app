7328595b307dc4451af8f77e9ce27a7df544f5c2
kay0829 <126226314+kay0829@users.noreply.github.com>
2023-06-23 17:47:11 +0900

    Revert "🔧 프로젝트 기본세팅"

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,25 @@
 import React from 'react';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-
-import Calendar from '@container/Calendar';
-import ErrorPage from '@container/ErrorPage';
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Calendar />,
-    errorElement: <ErrorPage />,
-  },
-]);
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    <RouterProvider router={router} />
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
