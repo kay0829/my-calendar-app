8616123279778f25f8a29d5148cd216a62d40170
kay0829 <eluka97@daum.net>
2023-06-23 18:52:49 +0900

    💄 1) MainCalendar 컴포넌트 추가

---
+++ src/component/Main/MainCalendar.tsx
@@ -0,0 +1,143 @@
+import React, { useState, useCallback } from 'react';
+import { Calendar, momentLocalizer } from 'react-big-calendar'
+import moment from 'moment';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+import '@styles/Main/customMainCalendar.css';
+
+moment.locale('ko-KR');
+const localizer = momentLocalizer(moment);
+
+interface IWeek {
+    [index: string]: string;
+    Sun: string;
+    Mon: string;
+    Tue: string;
+    Wed: string;
+    Thu: string;
+    Fri: string;
+    Sat: string;
+}
+
+interface IEvent {
+    allDay?: boolean | undefined;
+    title?: React.ReactNode | undefined;
+    start?: Date | undefined;
+    end?: Date | undefined;
+    resource?: any;
+}
+
+
+
+const events = [
+  {
+    id: 0,
+    allDay: false,
+    title: <p>Board meeting</p>,
+    start: new Date(2023, 5, 24, 9, 0, 0),
+    end: new Date(2023, 5, 24, 13, 0, 0),
+    resource: 1,
+  },
+  {
+    id: 1,
+    allDay: false,
+    title: <p>Board meeting</p>,
+    start: new Date(2023, 5, 23, 9, 0, 0),
+    end: new Date(2023, 5, 24, 9, 0, 0),
+    resource: 2,
+  },
+  {
+    id: 2,
+    allDay: false,
+    title: <p>Board meeting</p>,
+    start: new Date(2023, 5, 10, 9, 0, 0),
+    end: new Date(2023, 5, 11, 12, 0, 0),
+    resource: 3,
+  },
+  {
+    id: 11,
+    allDay: false,
+    title: <p>Board meeting</p>,
+    start: new Date(2023, 5, 29, 9, 0, 0),
+    end: new Date(2023, 5, 29, 12, 0, 0),
+    resource: 4,
+  },
+]
+
+function MainCalendar() {
+    const [myEvents, setEvents] = useState(events);
+
+    const handleSelectSlot = useCallback(
+      ({ start, end }: {start: Date, end: Date}) => {
+        const title = window.prompt('New Event name')
+        if (title) {
+          setEvents([...myEvents, { id: myEvents.length + 1, allDay: false, start, end, title: <p>{title}</p>, resource: myEvents.length + 1 }])
+        }
+      },
+      [setEvents]
+    )
+  
+    const handleSelectEvent = useCallback(
+      (event: IEvent) => window.alert(event.title),
+      []
+    )
+
+    const StyleNone = () => <></>;
+    const DateHeader = ({label, date}: {label: string, date: Date}) => {
+      const day = moment(date).format("M월 D일");
+      const isToday = moment().format("YYYYMD") === `${date.getFullYear()}${date.getMonth() + 1}${date.getDate()}`;
+
+      const DayElement = () => {
+        if (isToday) {
+          return (
+            <div className='bg-selected w-fit p-1 rounded-full cursor-pointer hover:bg-blue-600'>
+              <p className='text-xs text-center text-white font-normal'>{Number.parseInt(label)}</p>
+            </div>
+          )
+        }
+
+        return <p className='text-xs text-center cursor-pointer'>{label === '01' ? day : Number.parseInt(label)}</p>
+      }
+
+      return (
+        <DayElement />
+      )
+    };
+
+    const MonthHeader = ({label}: {label: string}) => {
+      const week: IWeek = {Sun: '일', Mon: '월', Tue: '화', Wed: '수', Thu: '목', Fri: '금', Sat: '토'};
+      return (
+        <p className='text-xs text-zinc-400'>{week[label] || ''}</p>
+      )
+    }
+
+    const EventElement = ({title}: {title: string}) => {
+      return (
+        <div>{title}</div>
+      )
+    }
+
+    return (
+      <div className='h-full'>
+          <Calendar
+            localizer={localizer}
+            startAccessor="start"
+            endAccessor="end"
+            events={myEvents}
+            selectable
+            onSelectEvent={handleSelectEvent}
+            onSelectSlot={handleSelectSlot}
+            components={{
+              toolbar: StyleNone,
+              dateCellWrapper: StyleNone,
+              month: {
+                header: MonthHeader,
+                dateHeader: DateHeader,
+                // event: EventElement,
+              }
+            }}
+          />
+      </div>
+    )
+}
+
+export default MainCalendar;
--- src/container/Main.tsx
+++ src/container/Main.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 
+import { Outlet } from "react-router-dom";
 
 import Gnb from "@component/Main/Gnb";
 import Sidebar from "@component/Main/Sidebar";
@@ -11,6 +12,9 @@ function Main() {
             <Gnb />
             <div className="flex-1 flex w-full">
                 <Sidebar />
+                <div className="flex-1 h-full">
+                    <Outlet />
+                </div>
                 <Aside />
             </div>
         </div>
