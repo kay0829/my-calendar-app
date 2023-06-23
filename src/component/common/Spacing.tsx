8a561b5056fda1d863a1017d13cc84349320e74f
kay0829 <eluka97@daum.net>
2023-06-23 18:31:19 +0900

    🔨 1) 컴포넌트 간 margin을 명확히 표현하기 위한 컴포넌트 Spacing 추가

---
+++ src/component/common/Spacing.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+/**
+ * 컴포넌트 간 margin을 명확히 표현하기 위한 컴포넌트
+ * @param space margin 관련 tailwindcss
+ * @returns 마진 크기만큼의 div
+ */
+function Spacing({space}: {space: string}) {
+    return (
+        <div className={`w-0 h-full ${space}`}></div>
+    )
+}
+
+export default Spacing;
