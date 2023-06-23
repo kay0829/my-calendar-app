1ae1cf4419a212bb26fbe3e481bc98ab0f60c829
kay0829 <eluka97@daum.net>
2023-06-23 18:52:00 +0900

    💄 1) Sidebar 컴포넌트 추가

--- src/container/Main.tsx
+++ src/container/Main.tsx
@@ -2,11 +2,15 @@ import React from "react";
 
 
 import Gnb from "@component/Main/Gnb";
+import Sidebar from "@component/Main/Sidebar";
 
 function Main() {
     return (
         <div className="box-border font-sans text-primary w-screen h-screen flex flex-col">
             <Gnb />
+            <div className="flex-1 flex w-full">
+                <Sidebar />
+            </div>
         </div>
     )
 }
